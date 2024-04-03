480a3a1fba1ff5a935babc90404b88f3d196369b
sina-razaghi <sina.razaghi79@gmail.com>
2024-04-03 10:06:56 +0330

    some news

--- Week_1/1_pandas.py
+++ Week_1/1_pandas.py
@@ -7,10 +7,33 @@ cites = {"ahvaz":32,
 
 temp = [32, 18, 22, 30]
 
+
+
 print("\nSeries Cites:")
-ds = pd.Series(cites)
-print(ds)
+ds1 = pd.Series(cites)
+print(ds1)
 
 print("\nSeries temp:")
 ds = pd.Series(temp, index=["ahvaz", "tehran", "shiraz", "kerman"])
 print(ds)
+
+print(ds.keys)
+print(ds.values)
+
+
+# print(cites.ahvaz) # => Wrongggggggggg
+print(ds1.ahvaz)
+
+print(ds.ahvaz)
+print(ds[0])
+
+print(ds[["ahvaz", "shiraz"]])
+print(ds[:2])
+print(ds[[0, 2]])
+
+print(ds[[True, False, True, False]])
+
+m = ds.mean()
+print(m)
+print(ds > m)
+
